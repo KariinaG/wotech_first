da34ed3099ba3a9340b9308fc1cca555e0cc6dbd
KariinaG <81989710+KariinaG@users.noreply.github.com>
2024-04-03 19:19:26 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args){
+
+        double temp = 5.5;
+
+        if (temp <= 5) {
+            System.out.println("Wear super warm");
+        }
+        else if (temp <= 15) {
+            System.out.println("Wear warm");
+        }
+        else if (temp <= 30) {
+            System.out.println("Wear normal");
+        }
+        else {
+            System.out.println("You need cooling");
+        }
+        }
+    }
